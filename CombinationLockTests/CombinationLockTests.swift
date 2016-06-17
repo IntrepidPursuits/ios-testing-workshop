e164700938cb77e3d2c1692d85e0f4af129d3d3b
Colin Brash <colin3@intrepid.io>
2016-06-17 08:01:13 -0400

    Add more tests

--- CombinationLockTests/CombinationLockTests.swift
+++ CombinationLockTests/CombinationLockTests.swift
@@ -16,4 +16,59 @@ class CombinationLockTests: XCTestCase {
         XCTAssertTrue(lock.locked)
     }
     
+    func testCorrectCombinationUnlocks() {
+        let lock = CombinationLock(combination: "123")
+        XCTAssertTrue(lock.locked)
+        lock.enterDigit1()
+        lock.enterDigit2()
+        lock.enterDigit3()
+        XCTAssertFalse(lock.locked)
+    }
+    
+    func testIncorrectCombinationDoesNotUnlock() {
+        let lock = CombinationLock(combination: "123")
+        XCTAssertTrue(lock.locked)
+        lock.enterDigit2()
+        XCTAssertTrue(lock.locked)
+        lock.enterDigit1()
+        XCTAssertTrue(lock.locked)
+        lock.enterDigit3()
+        XCTAssertTrue(lock.locked)
+    }
+    
+    func testEnteringDigitLocksIfUnlocked() {
+        let lock = CombinationLock(combination: "123")
+        XCTAssertTrue(lock.locked)
+        lock.enterDigit1()
+        lock.enterDigit2()
+        lock.enterDigit3()
+        XCTAssertFalse(lock.locked)
+        lock.enterDigit1()
+        XCTAssertTrue(lock.locked)
+    }
+
+    func testCorrectCombinationUnlocksAfterBeginningIncorrectCombination() {
+        let lock = CombinationLock(combination: "123")
+        XCTAssertTrue(lock.locked)
+        lock.enterDigit2()
+        lock.enterDigit2()
+        lock.enterDigit1()
+        lock.enterDigit2()
+        lock.enterDigit3()
+        XCTAssertFalse(lock.locked)
+    }
+    
+    func testEnteringCombinationTwiceInARowUnlocksTwice() {
+        let lock = CombinationLock(combination: "123")
+        XCTAssertTrue(lock.locked)
+        lock.enterDigit1()
+        lock.enterDigit2()
+        lock.enterDigit3()
+        XCTAssertFalse(lock.locked)
+        lock.enterDigit1()
+        XCTAssertTrue(lock.locked)
+        lock.enterDigit2()
+        lock.enterDigit3()
+        XCTAssertFalse(lock.locked)
+    }
 }
